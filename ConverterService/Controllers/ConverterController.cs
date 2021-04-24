9ef00155c5d2b1394236780777b679d17aa6b859
Sergey Podlesny <srgpdlsn@gmail.com>
2021-04-24 03:06:12 +0300

    add Controllers

---
+++ ConverterService/Controllers/ConverterController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConverterService.Controllers
+{
+    public class ConverterController : Controller
+    {
+        [HttpPost]
+        public string Speed()
+        {
+            return "Speed";
+        }
+
+        [HttpPost]
+        public string Lenght()
+        {
+            return "Lenght";
+        }
+
+        [HttpPost]
+        public string Temperature()
+        {
+            return "Temperature";
+        }
+
+        [HttpPost]
+        public string Weight()
+        {
+            return "Weight";
+        }
+
+    }
+}
---
+++ ConverterService/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ConverterService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+    }
+
+}
